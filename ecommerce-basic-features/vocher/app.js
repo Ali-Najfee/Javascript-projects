079d80ac5fb659f99fd7c3f5a1b7ccc8500f2905
Ali-Najfee <expastudio1@gmail.com>
2024-12-25 12:51:13 +0500

    new project

---
+++ ecommerce-basic-features/vocher/app.js
@@ -0,0 +1,10 @@
+let vocuher=[
+  {
+    orderNum:1,name:"Ali Najfee",intro:"I am senior web app devloper with 4 years of experience in tech gaint industry",services1:"Web Services",services2:"App Services",services3:"Web AppServices"
+  }
+]
+function voucherGenerator(){
+const voucherGen =document.querySelector('.voucherBox');
+
+
+}
